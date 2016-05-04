39b0c0cc48e4590a088abfba8fbc222adb5978e6
Stein Eldar Johnsen <morimekta@gmail.com>
2016-05-05 00:58:16 +0200

    Test against a providence servlet too.

---
+++ providence-rpc-tool/src/test/providence/test.thrift
@@ -0,0 +1,28 @@
+namespace java net.morimekta.test.providence
+
+
+struct Request {
+    1: string text;
+}
+
+struct Response {
+    1: string text;
+}
+
+exception Failure {
+    1: string text;
+}
+
+service MyService {
+    oneway void ping();
+
+    Response test(1: Request request) throws (1: Failure f);
+
+    double test2(2: i64 a, 4: byte late);
+}
+
+service MyService2 {
+    Response testing(1: Request request) throws (1: Failure f);
+
+    double test2(2: i64 a, 4: byte late);
+}
