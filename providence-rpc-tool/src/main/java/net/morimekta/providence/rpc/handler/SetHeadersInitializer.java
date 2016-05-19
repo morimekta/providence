5b54629c85de565f480ef2a8fbbf887155e8d4b5
Stein Eldar Johnsen <morimekta@gmail.com>
2016-05-19 19:26:44 +0200

    Handle more special cases for headers.

--- providence-rpc-tool/src/main/java/net/morimekta/providence/rpc/handler/SetHeadersInitializer.java
+++ providence-rpc-tool/src/main/java/net/morimekta/providence/rpc/handler/SetHeadersInitializer.java
@@ -1,5 +1,6 @@
 package net.morimekta.providence.rpc.handler;
 
+import com.google.api.client.http.HttpHeaders;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestInitializer;
 
@@ -7,7 +8,7 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * Request initializer that set's a set of fixed headers.
+ * Request initializer that sets a set of fixed headers.
  */
 public class SetHeadersInitializer implements HttpRequestInitializer {
     private final Map<String, String> headers;
@@ -18,16 +19,30 @@ public class SetHeadersInitializer implements HttpRequestInitializer {
 
     @Override
     public void initialize(HttpRequest request) throws IOException {
+        HttpHeaders http = request.getHeaders();
         for (Map.Entry<String, String> entry : headers.entrySet()) {
-            switch (entry.getKey()
-                         .toLowerCase()) {
+            String value = entry.getValue();
+            switch (entry.getKey().toLowerCase()) {
+                case "accept":
+                    http.setAccept(value);
+                    break;
+                case "accept-encoding":
+                    http.setAcceptEncoding(value);
+                    break;
                 case "authorization":
-                    request.getHeaders()
-                           .setAuthorization(entry.getValue());
+                    http.setAuthorization(value);
+                    break;
+                case "content-encoding":
+                    http.setContentEncoding(value);
+                    break;
+                case "content-type":
+                    http.setContentType(value);
+                    break;
+                case "user-agent":
+                    http.setUserAgent(value);
                     break;
                 default:
-                    request.getHeaders()
-                           .set(entry.getKey(), entry.getValue());
+                    http.set(entry.getKey(), value);
                     break;
             }
         }
