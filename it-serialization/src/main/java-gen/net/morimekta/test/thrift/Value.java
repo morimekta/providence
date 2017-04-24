5b9e20c78849edba60a4c22268f8a6e0cdc2c89b
Stein Eldar Johnsen <oss@morimekta.net>
2017-04-24 19:59:54 +0200

    Regenerate pre-generated thrift.

--- it-serialization/src/main/java-gen/net/morimekta/test/thrift/Value.java
+++ it-serialization/src/main/java-gen/net/morimekta/test/thrift/Value.java
@@ -6,6 +6,11 @@
  */
 package net.morimekta.test.thrift;
 
+
+import java.util.Map;
+import java.util.HashMap;
+import org.apache.thrift.TEnum;
+
 public enum Value implements org.apache.thrift.TEnum {
   FIRST(1),
   SECOND(2),
