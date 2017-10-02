2cab89f3ae7c48db0754e2463c7510cbce8a4b64
Stein Eldar Johnsen <oss@morimekta.net>
2017-10-02 20:36:28 +0200

    Fix comment relating HTTP to be more generic.

--- providence-core/src/main/java/net/morimekta/providence/util/ServiceCallInstrumentation.java
+++ providence-core/src/main/java/net/morimekta/providence/util/ServiceCallInstrumentation.java
@@ -14,17 +14,21 @@ public interface ServiceCallInstrumentation {
      * After each service call this method is called with the duration, call and
      * response objects. Exceptions from the call is ignored, and will not affect
      * the response in any way.
+     * <p>
+     * Note that the timing may not include the whole stack time since receiving
+     * the first packet, that is dependent on the specific implementation and the
+     * limitations there. E.g. it does not include the time receiving the first
+     * HTTP packet with the headers, or waiting for free worker threads when using
+     * <code>ProvidenceServlet</code>.
      *
      * @param duration The duration of handling the service call in milliseconds,
-     *                 including receiving and sending it. It does not include the
-     *                 time receiving the first HTTP packet with the headers, or
-     *                 waiting for free worker threads.
+     *                 including receiving and sending it.
      * @param call     The call triggered.
-     * @param response The response returned.
+     * @param reply    The reply returned.
      */
     void afterCall(double duration,
                    @Nullable PServiceCall call,
-                   @Nullable PServiceCall response);
+                   @Nullable PServiceCall reply);
 
     /**
      * Handy constant for calculating MS duration.
