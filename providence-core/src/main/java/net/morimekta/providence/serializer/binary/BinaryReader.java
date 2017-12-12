2b60a1c9dd98ac4bb365ad205bab98818a05928f
Stein Eldar Johnsen <steineldar@zedge.net>
2017-12-12 10:55:15 +0100

    Document BinaryReader and BinaryWriter methods.
    As not intended for direct use.

--- providence-core/src/main/java/net/morimekta/providence/serializer/binary/BinaryReader.java
+++ providence-core/src/main/java/net/morimekta/providence/serializer/binary/BinaryReader.java
@@ -1,13 +1,27 @@
 package net.morimekta.providence.serializer.binary;
 
+import net.morimekta.providence.descriptor.PMessageDescriptor;
 import net.morimekta.util.io.BigEndianBinaryReader;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Interface for message builders that can read binary directly from
  * an big endian binary reader.
  */
 public interface BinaryReader {
+    /**
+     * Read the binary content into the current builder.
+     *
+     * NOTE: This method is not intended to be used directly. Instead use
+     * the {@link net.morimekta.providence.serializer.BinarySerializer#deserialize(InputStream, PMessageDescriptor)}
+     * call.
+     *
+     * @param reader The reader to read from.
+     * @param strict If content should be handled strictly. True means to fail
+     *               on everything that Apache thrift failed read() on.
+     * @throws IOException When unable to read message for any reason.
+     */
     void readBinary(BigEndianBinaryReader reader, boolean strict) throws IOException;
 }
--- providence-core/src/main/java/net/morimekta/providence/serializer/binary/BinaryWriter.java
+++ providence-core/src/main/java/net/morimekta/providence/serializer/binary/BinaryWriter.java
@@ -1,12 +1,25 @@
 package net.morimekta.providence.serializer.binary;
 
+import net.morimekta.providence.PMessage;
 import net.morimekta.util.io.BigEndianBinaryWriter;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Interface for messages that can directly be written to binary.
  */
 public interface BinaryWriter {
+    /**
+     * Write the current message to the binary writer.
+     *
+     * NOTE: This method is not intended to be used directly. Instead use
+     * the {@link net.morimekta.providence.serializer.BinarySerializer#serialize(OutputStream, PMessage)}
+     * call.
+     *
+     * @param writer The binary writer to write to.
+     * @return The number of bytes written.
+     * @throws IOException If it failed to write the message for any reason.
+     */
     int writeBinary(BigEndianBinaryWriter writer) throws IOException;
 }
