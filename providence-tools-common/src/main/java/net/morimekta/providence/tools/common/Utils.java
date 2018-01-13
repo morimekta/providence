0d1f0fa79d2cf381ed15b98ada4f9bec28f6c749
Stein Eldar Johnsen <oss@morimekta.net>
2018-01-13 15:27:32 +0100

    Read version string only once.

--- providence-tools-common/src/main/java/net/morimekta/providence/tools/common/Utils.java
+++ providence-tools-common/src/main/java/net/morimekta/providence/tools/common/Utils.java
@@ -2,17 +2,27 @@ package net.morimekta.providence.tools.common;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.util.Properties;
 
 /**
  * General utility methods.
  */
 public class Utils {
-    public static String getVersionString() throws IOException {
-        Properties properties = new Properties();
-        try (InputStream in = Utils.class.getResourceAsStream("/version.properties")) {
-            properties.load(in);
+    private transient volatile static String versionString = null;
+
+    public static String getVersionString() {
+        if (versionString == null) {
+            try {
+                Properties properties = new Properties();
+                try (InputStream in = Utils.class.getResourceAsStream("/version.properties")) {
+                    properties.load(in);
+                }
+                versionString = "v" + properties.getProperty("build.version");
+            } catch (IOException e) {
+                throw new UncheckedIOException(e.getMessage(), e);
+            }
         }
-        return "v" + properties.getProperty("build.version");
+        return versionString;
     }
 }
