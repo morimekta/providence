f2b05d86dd7af784d8891c6967aa8f04028140a4
Stein Eldar Johnsen <oss@morimekta.net>
2018-05-01 10:46:09 +0200

    Update javadoc trigger semver check.

--- trigger-javadoc.sh
+++ trigger-javadoc.sh
@@ -3,7 +3,7 @@ VERSION=${1:-}
 
 if [[ -z "$1" ]]
 then
-  VERSION=$(git tag | grep '^v[0-9]' | sort -V | tail -n 1 | sed 's/^v//')
+  VERSION=$(git tag | grep '^v[0-9]\+[.][0-9]\+[.][0-9]\+' | sort -V | tail -n 1 | sed 's/^v//')
 fi
 
 for m in providence $(\
@@ -15,5 +15,5 @@ for m in providence $(\
 do
   URL="http://www.javadoc.io/doc/net.morimekta.providence/$m/$VERSION"
   echo -e "\033[01m$URL\033[00m"
-  curl -s $URL > /dev/null || exit 1
+  curl -s ${URL} > /dev/null || exit 1
 done
